ec77b586e24559a4689f1be2796964bc737003d1
Libter <libter.pl@gmail.com>
2018-08-12 13:37:22 +0200

    Hello Craftlin!

---
+++ src/net/craftlin/plugin/bukkit/BukkitCraftlin.kt
@@ -0,0 +1,11 @@
+package net.craftlin.plugin.bukkit
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class BukkitCraftlin: JavaPlugin() {
+
+    override fun onEnable() {
+        println("Hello Craftlin!")
+    }
+
+}
