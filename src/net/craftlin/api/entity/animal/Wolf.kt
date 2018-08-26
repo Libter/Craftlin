6e5e48193d2632f4900f032d215a9a51c1b4fb9a
Libter <libter.pl@gmail.com>
2018-08-26 14:11:17 +0200

    Add TameableEntity interface

--- src/net/craftlin/api/entity/animal/Wolf.kt
+++ src/net/craftlin/api/entity/animal/Wolf.kt
@@ -1,8 +1,9 @@
 package net.craftlin.api.entity.animal
 
 import net.craftlin.api.entity.base.GrowingEntity
+import net.craftlin.api.entity.base.TameableEntity
 
-interface Wolf: GrowingEntity {
+interface Wolf: GrowingEntity, TameableEntity {
     var collarColor: String
     var angry: Boolean
 }
