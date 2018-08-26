cc31f3ac96889109314496edd41faeaa84028875
Libter <libter.pl@gmail.com>
2018-08-26 16:11:10 +0200

    Add mockito and write first test for Bukkit implementation!

---
+++ test/net/craftlin/test/BukkitPlayerTest.kt
@@ -0,0 +1,59 @@
+package net.craftlin.test
+
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doAnswer
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import net.craftlin.bukkit.impl.entity.BukkitPlayer
+import org.bukkit.GameMode
+import org.bukkit.entity.Player
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.util.UUID
+
+class BukkitPlayerTest {
+
+    private val uuid = UUID.randomUUID()
+    private val origin: Player = mock {
+        var mode = GameMode.SURVIVAL
+        on { name } doReturn "tester"
+        on { uniqueId } doReturn uuid
+        on { isOnline } doReturn true
+        on { gameMode } doAnswer { mode }
+        on { gameMode = any() } doAnswer { mode = it.getArgument<GameMode>(0); Unit }
+    }
+    private val player = BukkitPlayer(origin)
+
+    @Test
+    fun testName() {
+        assertEquals("tester", player.name)
+        player.name = "x" //player name should be immutable
+        assertEquals("tester", player.name)
+    }
+
+    @Test
+    fun testGameMode() {
+        assertEquals("survival", player.gamemode)
+        player.gamemode = "creative"
+        assertEquals(GameMode.CREATIVE, origin.gameMode)
+        origin.gameMode = GameMode.SPECTATOR
+        assertEquals("spectator", player.gamemode)
+    }
+
+    @Test
+    fun testVariables() {
+        assertEquals(uuid.toString(), player.uuid)
+        assertEquals(true, player.online)
+    }
+
+    @Test
+    fun testFunctions() {
+        player.message("test")
+        verify(origin).sendMessage("test")
+
+        player.kick("test")
+        verify(origin).kickPlayer("test")
+    }
+
+}
