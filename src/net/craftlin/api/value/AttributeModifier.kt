33c5598c462084c06c2b8320c95cf506ea5b6365
Libter <libter.pl@gmail.com>
2018-08-28 21:06:28 +0200

    Test driven attribute modifiers!

--- src/net/craftlin/api/value/AttributeModifier.kt
+++ src/net/craftlin/api/value/AttributeModifier.kt
@@ -2,6 +2,7 @@ package net.craftlin.api.value
 
 interface AttributeModifier {
 
+    fun remove()
     operator fun plusAssign(value: Double)
     operator fun minusAssign(value: Double)
     operator fun timesAssign(value: Double)
--- src/net/craftlin/bukkit/impl/value/BukkitAttributeModifier.kt
+++ src/net/craftlin/bukkit/impl/value/BukkitAttributeModifier.kt
@@ -6,18 +6,28 @@ import org.bukkit.attribute.AttributeModifier.Operation
 
 class BukkitAttributeModifier(private val instance: AttributeInstance, private val name: String): AttributeModifier {
 
-    private fun raw(rawValue: Double, operation: Operation) {
-        val modifier = org.bukkit.attribute.AttributeModifier(name, rawValue, operation)
-        if (instance.modifiers.removeIf { it.name == name && it != modifier }) {
+    private fun raw(suffix: String, rawValue: Double, operation: Operation) {
+        val localName = "$name-$suffix"
+        val modifier = org.bukkit.attribute.AttributeModifier(localName, rawValue, operation)
+        var equals = false
+        instance.modifiers.removeIf {
+            equals = equals || it == modifier;
+            it.name == localName && it != modifier
+        }
+        if (!equals) {
             instance.addModifier(modifier)
         }
     }
 
-    override fun plusAssign(value: Double) = raw(value, Operation.ADD_NUMBER)
+    override fun remove() {
+        instance.modifiers.removeIf { it.name.startsWith(name) }
+    }
 
-    override fun minusAssign(value: Double) = plusAssign(-value)
+    override fun plusAssign(value: Double) = raw("plus", value, Operation.ADD_NUMBER)
 
-    override fun timesAssign(value: Double) = raw(value - 1, Operation.MULTIPLY_SCALAR_1)
+    override fun minusAssign(value: Double) = raw("minus", -value, Operation.ADD_NUMBER)
 
-    override fun divAssign(value: Double) = timesAssign(1 / value)
+    override fun timesAssign(value: Double) = raw("times", value - 1, Operation.MULTIPLY_SCALAR_1)
+
+    override fun divAssign(value: Double) = raw("div", (1 / value) - 1, Operation.MULTIPLY_SCALAR_1)
 }
