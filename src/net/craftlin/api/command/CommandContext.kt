ef2957f8ddf85c4594461385c64e38c0d8ca2b70
Reedzev_ <mikolaj1440@hotmail.com>
2018-08-31 18:06:08 +0200

    Added docs for CommandContext

--- src/net/craftlin/api/command/CommandContext.kt
+++ src/net/craftlin/api/command/CommandContext.kt
@@ -14,6 +14,9 @@ abstract class CommandContext(private val command: Command, private val args: Ar
         private val decimalRefex = Regex("^\\d+(.\\d+)?$")
     }
 
+    /**
+     * The command sender
+     */
     abstract val sender: Sender
 
     protected val exception = CommandUsageException(command)
@@ -25,24 +28,51 @@ abstract class CommandContext(private val command: Command, private val args: Ar
 
     protected fun forceGet(key: String) = get(key) ?: throw exception
 
+    /**
+     * Is an argument (and its [key]) defined?
+     */
     fun defined(key: String) = get(key) != null
 
+    /**
+     * Returns a text specified in the argument with key [key]
+     *
+     * @throws CommandUsageException when the argument is unset or key is invalid
+     */
     fun text(key: String) = forceGet(key)
 
+    /**
+     * Returns a number specified in the argument with key [key]
+     *
+     * @throws CommandUsageException when the argument is unset or key is invalid
+     */
     fun number(key: String): BigInteger {
         val text = text(key)
         if (!text.matches(numberRegex)) throw exception
         return BigInteger(text)
     }
 
+    /**
+     * Returns a decimal specified in the argument with key [key]
+     *
+     * @throws CommandUsageException when the argument is unset or key is invalid
+     */
     fun decimal(key: String): BigDecimal {
         val text = text(key)
         if (!text.matches(decimalRefex)) throw exception
         return BigDecimal(text)
     }
 
+    /**
+     * Returns the player with name specified in the argument with key [key]
+     *
+     * @throws CommandUsageException when the argument is unset or key is invalid
+     */
     abstract fun player(key: String): Player
 
+    /**
+     * Retrieves the offline player with name specified in the argument with key [key]
+     *
+     * @throws CommandUsageException when the argument is unset or key is invalid
+     */
     abstract fun offlinePlayer(key: String, callback: itF<OfflinePlayer?>)
-
 }
