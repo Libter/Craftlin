3f5afed80714024e9de7ad0e2fef24f4b77fbfaa
Reedzev_ <mikolaj1440@hotmail.com>
2018-09-01 15:23:43 +0200

    '&' are now replaced to '&' in messages

--- src/net/craftlin/bukkit/impl/entity/BukkitConsole.kt
+++ src/net/craftlin/bukkit/impl/entity/BukkitConsole.kt
@@ -1,10 +1,11 @@
 package net.craftlin.bukkit.impl.entity
 
 import net.craftlin.api.entity.Sender
+import org.bukkit.ChatColor
 import org.bukkit.command.ConsoleCommandSender
 
 class BukkitConsole(private val origin: ConsoleCommandSender): Sender {
     override fun message(message: String) {
-        origin.sendMessage(message)
+        origin.sendMessage(ChatColor.translateAlternateColorCodes('&', message))
     }
 }
--- src/net/craftlin/bukkit/impl/entity/BukkitPlayer.kt
+++ src/net/craftlin/bukkit/impl/entity/BukkitPlayer.kt
@@ -3,6 +3,7 @@ package net.craftlin.bukkit.impl.entity
 import net.craftlin.api.entity.Player
 import net.craftlin.bukkit.impl.entity.base.BukkitLivingEntity
 import net.craftlin.bukkit.impl.value.BukkitGameMode
+import org.bukkit.ChatColor
 import org.bukkit.GameMode
 
 class BukkitPlayer(private val origin: org.bukkit.entity.Player): BukkitLivingEntity(origin), Player  {
@@ -19,7 +20,7 @@ class BukkitPlayer(private val origin: org.bukkit.entity.Player): BukkitLivingEn
 
     override var gamemode by BukkitGameMode.Delegate(::originGameMode)
 
-    override fun message(message: String) = origin.sendMessage(message)
+    override fun message(message: String) = origin.sendMessage(ChatColor.translateAlternateColorCodes('&', message))
 
     override fun kick(reason: String) = origin.kickPlayer(reason)
 
